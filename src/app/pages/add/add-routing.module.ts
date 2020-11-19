23443056b2ac04a780fff0da88b6de586ac2e7e1
Daniel Kurin <dkurin@swiftlet.technology>
2020-11-19 10:23:22 -0500

    Create 'Add' page stubs

--- src/app/pages/add/add-routing.module.ts
+++ src/app/pages/add/add-routing.module.ts
@@ -7,6 +7,22 @@ const routes: Routes = [
   {
     path: '',
     component: AddPage
+  },
+  {
+    path: 'recipes',
+    loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
+  },
+  {
+    path: 'reviews',
+    loadChildren: () => import('./reviews/reviews.module').then( m => m.ReviewsPageModule)
+  },
+  {
+    path: 'deals',
+    loadChildren: () => import('./deals/deals.module').then( m => m.DealsPageModule)
+  },
+  {
+    path: 'tips',
+    loadChildren: () => import('./tips/tips.module').then( m => m.TipsPageModule)
   }
 ];
 
