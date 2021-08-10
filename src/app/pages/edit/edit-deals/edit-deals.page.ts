f34fbf82952c6a094d9143f899c94f5ea6d9d54d
Dan Kurin <dkurin@swiftlet.technology>
2021-08-09 22:07:20 -0400

    Get Deals to show blank date if none selected

--- src/app/pages/edit/edit-deals/edit-deals.page.ts
+++ src/app/pages/edit/edit-deals/edit-deals.page.ts
@@ -63,7 +63,7 @@ export class EditDealsPage implements OnInit {
     this.dealForm.get('title').setValue(deal.title);
     this.dealForm.get('description').setValue(deal.text);
     this.dealForm.get('startDate').setValue((<EatsDate>deal.startDate).toDate().toISOString());
-    if((<EatsDate>deal.endDate).epochSecond !== BigInt(0)) {
+    if((<EatsDate>deal.endDate).epochSecond != BigInt(0)) {
       this.dealForm.get('endDate').setValue((<EatsDate>deal.endDate).toDate().toISOString());
     }
     this.dealForm.get('price').setValue(deal.price);
