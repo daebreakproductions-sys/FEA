8ea22e0c094297233be02710c163d1b4418f81e6
Daniel Kurin <dantheman2865@users.noreply.github.com>
2021-12-04 14:49:01 -0500

    iOS Changes
    Updates to accommodate iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,17 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myepicidea.flinteats',
+  appName: 'Flint Eats',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: false,
+    },
+   },
+   server: {
+       iosScheme: 'ionic'
+   }
+};
+
+export default config;
